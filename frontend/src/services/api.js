aef13425524c0ea1dce9346dc018b2ed5627fbeb
Yajandave <yajandave3@gmail.com>
2026-04-29 18:34:51 +0100

    Make frontend API URL configurable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = (import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000").replace(/\/$/, "");
 
 function buildUrl(path, filters = {}) {
   const url = new URL(`${API_BASE}${path}`);
